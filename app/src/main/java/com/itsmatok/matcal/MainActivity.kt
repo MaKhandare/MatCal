78f11591dd033a0c9cda5a450991fd18d8aedd6b
Matthäus Khandare <khandare.matthaeus@proton.me>
2025-12-09 00:00:43 +0100

    chore: add simple calendar screen

--- app/src/main/java/com/itsmatok/matcal/MainActivity.kt
+++ app/src/main/java/com/itsmatok/matcal/MainActivity.kt
@@ -4,26 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Info
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -31,7 +11,7 @@ import com.itsmatok.matcal.ui.theme.MatCalTheme
 import kotlinx.serialization.Serializable
 
 @Serializable
-object Home
+object Calendar
 
 @Serializable
 object License
@@ -45,59 +25,16 @@ class MainActivity : ComponentActivity() {
                 val navController = rememberNavController()
 
                 NavHost(
-                    navController = navController, startDestination = Home
+                    navController = navController, startDestination = Calendar
                 ) {
-                    composable<Home> {
-                        HomeScreen(onNavigateLicense = { navController.navigate(License) })
+                    composable<Calendar> {
+                        CalendarScreen()
                     }
 
                     composable<License> {
-                        LicenseScreen(onNavigateBack = { navController.navigate(Home) })
+                        LicenseScreen(onNavigateBack = { navController.navigate(Calendar) })
                     }
                 }
-            }
-        }
-    }
-}
-
-
-@Composable
-fun HomeScreen(
-    modifier: Modifier = Modifier,
-    onNavigateLicense: () -> Unit = {}
-) {
-    Scaffold(
-        modifier = modifier
-    ) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .padding(innerPadding)
-                .fillMaxSize()
-                .padding(24.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-
-            Text(
-                text = "MatCal",
-                style = MaterialTheme.typography.displayMedium,
-                fontWeight = FontWeight.Bold,
-                color = MaterialTheme.colorScheme.primary
-            )
-
-            Spacer(modifier = Modifier.height(48.dp))
-
-            Button(
-                onClick = onNavigateLicense,
-                contentPadding = ButtonDefaults.ContentPadding
-            ) {
-                Icon(
-                    imageVector = Icons.Outlined.Info,
-                    contentDescription = null,
-                    modifier = Modifier.size(18.dp)
-                )
-                Spacer(modifier = Modifier.size(8.dp))
-                Text("Open Source Licenses")
             }
         }
     }
