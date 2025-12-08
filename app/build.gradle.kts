fc2dd2a10166d9ace3b85baa207a5038144f8305
Matthäus Khandare <khandare.matthaeus@proton.me>
2025-12-09 00:00:15 +0100

    chore: add calendar compose and extended material icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,8 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.aboutlibraries.compose)
     implementation(libs.androidx.navigation.compose)
+    implementation("com.kizitonwose.calendar:compose:2.9.0")
+    implementation(libs.androidx.compose.material.icons.extended)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
